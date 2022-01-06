17b199b1906ddc95e128c543894f26bf4f9a6e2e
gaurdian2701 <55644010+gaurdian2701@users.noreply.github.com>
2022-01-06 18:30:21 +0530

    added admin details page

--- src/main/java/com/helpdesk/controller/AdminController.java
+++ src/main/java/com/helpdesk/controller/AdminController.java
@@ -112,5 +112,12 @@ public class AdminController {
     	return "admin/users";
     }
     
+    @RequestMapping(value = "account", method = RequestMethod.GET)
+    public String userSettings(Model model, @RequestParam("id") int id){
+    	User user = new User();
+    	user = userRepository.findById(id);
+    	model.addAttribute("user",user);
+        return "user/stats";
+    }
 
 }
